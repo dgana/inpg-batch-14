7b5f806df5b603d5909933cfaf95e74256eda807
dgana <dhegana@qlue.id>
2019-05-13 20:20:55 +0700

    add day4

---
+++ day4/index.js
@@ -0,0 +1,49 @@
+/*
+
+SOAL 1
+
+Grade A = > 85
+Grade B = > 75 & < 85
+GRADE C = > 65 & < 75
+GRADE D = < 65
+
+Tampilkan Grade Siswa sesuai nilai yang didapat
+
+Buatlah PSEUDOCODE
+*/
+
+/*
+
+SOAL 2
+
+Sebuah restoran hanya dapat menampung 50 orang
+setiap meja hanya dapat menampung 3 orang
+
+Tugas:
+Hitung sisa orang yang tidak duduk dan 
+berapa meja yang terpakai!
+
+Buatlah PSEUDOCODE
+*/
+
+/*
+
+/*
+
+SOAL 3
+
+STORE "kecepatan" to 20
+STORE "penumpang" to 0
+
+IF "penumpang" > 2
+  MIN "kecepatan" by 5
+
+ELSE IF "penumpang" = 4
+  MIN "kecepatan" by 7
+
+ELSE
+  MIN "kecepatan" by 2
+  
+DISPLAY "kecepatan"
+
+*/
