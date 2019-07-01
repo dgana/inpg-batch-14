d2fb84730c38a0f939528ff8ae8f7281d9543ef5
dgana <dhegana@qlue.id>
2019-07-01 20:43:50 +0700

    add day15

---
+++ day15/latihan1.js
@@ -0,0 +1,11 @@
+function threeStepsAB (text) {
+  var regex = /[a|b]...[a|b]/g
+  return regex.test(text)
+}
+
+// TEST CASES
+console.log(threeStepsAB('lane borrowed')); // true
+console.log(threeStepsAB('i am sick')); // false
+console.log(threeStepsAB('you are boring')); // true
+console.log(threeStepsAB('barbarian')); // true
+console.log(threeStepsAB('bacon and meat')); // false
---
+++ day15/latihan3.js
@@ -0,0 +1,17 @@
+function isPrime (number) {
+  for (var i = number - 1; i > 1; i--) {
+    if (number % i === 0) {
+      return false
+    }
+  }
+  return true
+}
+
+// TEST CASES
+console.log(isPrime(3)); // true
+console.log(isPrime(7)); // true
+console.log(isPrime(6)); // false
+console.log(isPrime(23)); // true
+console.log(isPrime(33)); // false
+
+
